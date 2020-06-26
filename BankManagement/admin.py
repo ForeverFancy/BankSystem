be5b5cf5c6e117be74d0beec80babd9a9bfc1b82
ForeverFancy <1026816510@qq.com>
2020-06-26 16:21:15 +0800

    [code] register models.

--- BankManagement/admin.py
+++ BankManagement/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from BankManagement.models import *
 
 # Register your models here.
+admin.site.register(Bank)
+admin.site.register(CheckAccount)
+admin.site.register(Department)
+admin.site.register(Employee)
+admin.site.register(Customer)
+admin.site.register(CustomerToCA)
+admin.site.register(Loan)
+admin.site.register(CustomerToLoan)
+admin.site.register(SavingAccount)
+admin.site.register(CustomerToSA)
+admin.site.register(LoanRelease)
--- BankSystem/settings.py
+++ BankSystem/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'BankManagement'
+    'BankManagement.apps.BankmanagementConfig'
 ]
 
 MIDDLEWARE = [
@@ -116,7 +116,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
